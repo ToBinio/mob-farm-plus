4a7f6379e509226dee3c2e5a791d7053990908dc
tobinio <Tobias.frischmann1@gmail.com>
2024-08-25 15:05:56 +0200

    Initial commit

---
+++ src/client/java/tobinio/mobfarmplus/MobFarmPlusClient.java
@@ -0,0 +1,10 @@
+package tobinio.mobfarmplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MobFarmPlusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
