7104e90bbec49dd0a964b296f184e084ddf0a62d
tobinio <Tobias.frischmann1@gmail.com>
2024-08-28 13:17:08 +0200

    dev fan model

--- src/client/java/tobinio/mobfarmplus/MobFarmPlusClient.java
+++ src/client/java/tobinio/mobfarmplus/MobFarmPlusClient.java
@@ -1,11 +1,16 @@
 package tobinio.mobfarmplus;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import tobinio.mobfarmplus.blocks.ModBlocks;
 import tobinio.mobfarmplus.particle.ModParticles;
 
 public class MobFarmPlusClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ModParticles.initialize();
+
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.FAN, RenderLayer.getCutout());
     }
 }
